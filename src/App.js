da1287d9b9902f2c65c45a38a3e80196f8296ecd
akib22 <adnanakib75@gmail.com>
2022-02-10 02:05:43 +0600

    Initialize router navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Wishlist from './pages/Wishlist';
+
 function App() {
   return (
     <div className="App">
-      <p>Assignment frontend</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
