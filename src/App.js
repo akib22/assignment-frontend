4dbba9485944c45b92bed1e55f2a68d80ade9c01
akib22 <adnanakib75@gmail.com>
2022-02-10 21:53:41 +0600

    Set user and access token in context
    When first render the app read data from localStroage and set it to the user context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import { useUser } from './contexts/user';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -7,8 +9,21 @@ import Search from './pages/Search';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Wishlist from './pages/Wishlist';
+import { getDataFromLocalStorage } from './utils/localStroage';
 
 function App() {
+  const [, dispatch] = useUser();
+
+  useEffect(() => {
+    const user = getDataFromLocalStorage('user', null);
+    const accessToken = getDataFromLocalStorage('accessToken', null);
+
+    dispatch({
+      type: 'setUser',
+      payload: { user, accessToken },
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header />
