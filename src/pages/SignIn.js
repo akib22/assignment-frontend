ae89a27c0772bed739ec6a1d7c4580480cad3679
akib22 <adnanakib75@gmail.com>
2022-02-10 22:53:34 +0600

    Control sign-in and sign-up form using react-hook-form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,20 +1,80 @@
 import { Container, Form, Button, Col, Card } from 'react-bootstrap';
+import { useForm, Controller } from 'react-hook-form';
 
 export default function SignIn() {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSignIn = (data) => {
+    // TODO: call api to sign a user
+    reset();
+  };
+
   return (
     <Container>
       <Col className="mx-auto" xs={12} sm={10} md={6} lg={6} xl={4}>
         <Card className="mt-5 p-3">
           <h2 className="text-center mb-3">Sign In</h2>
-          <Form>
+          <Form onSubmit={handleSubmit(onSignIn)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Controller
+                control={control}
+                name="email"
+                rules={{ required: 'Email is required.' }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.email}
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  );
+                }}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Controller
+                control={control}
+                name="password"
+                rules={{ required: 'Password is required.', minLength: 6 }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.password}
+                      type="password"
+                      placeholder="Enter password"
+                    />
+                  );
+                }}
+              />
+              {errors?.password?.type === 'required' && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.password?.message}
+                </Form.Control.Feedback>
+              )}
+              {errors?.password?.type === 'minLength' && (
+                <Form.Control.Feedback type="invalid">
+                  Password should be 6 characters.
+                </Form.Control.Feedback>
+              )}
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,30 +1,137 @@
 import { Container, Form, Button, Col, Card } from 'react-bootstrap';
+import { useForm, Controller } from 'react-hook-form';
 
 export default function SignUp() {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSignUp = (data) => {
+    // TODO: call api to signup a user
+    reset();
+  };
+
   return (
     <Container>
       <Col className="mx-auto" xs={12} sm={10} md={6} lg={6} xl={4}>
         <Card className="mt-5 p-3">
           <h2 className="text-center mb-3">Sign Up</h2>
-          <Form>
+          <Form onSubmit={handleSubmit(onSignUp)}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Controller
+                control={control}
+                name="name"
+                rules={{ required: 'Name is required.', minLength: 4 }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.name}
+                      type="name"
+                      placeholder="Enter name"
+                    />
+                  );
+                }}
+              />
+              {errors?.name?.type === 'required' && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.name?.message}
+                </Form.Control.Feedback>
+              )}
+              {errors?.name?.type === 'minLength' && (
+                <Form.Control.Feedback type="invalid">
+                  Name should be 4 characters.
+                </Form.Control.Feedback>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter your email" />
+              <Controller
+                control={control}
+                name="email"
+                rules={{ required: 'Email is required.' }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.email}
+                      type="email"
+                      placeholder="Enter email"
+                    />
+                  );
+                }}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="phone">
               <Form.Label>Phone number</Form.Label>
-              <Form.Control type="text" placeholder="Enter your phone number" />
+              <Controller
+                control={control}
+                name="phoneNumber"
+                rules={{ required: 'Phone number is required.' }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.phoneNumber}
+                      type="phoneNumber"
+                      placeholder="Enter phoneNumber"
+                    />
+                  );
+                }}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.phoneNumber?.message}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Controller
+                control={control}
+                name="password"
+                rules={{ required: 'Password is required.', minLength: 6 }}
+                defaultValue=""
+                render={({ field }) => {
+                  return (
+                    <Form.Control
+                      onChange={field.onChange}
+                      value={field.value}
+                      ref={field.ref}
+                      isInvalid={errors.password}
+                      type="password"
+                      placeholder="Enter password"
+                    />
+                  );
+                }}
+              />
+              {errors?.password?.type === 'required' && (
+                <Form.Control.Feedback type="invalid">
+                  {errors.password?.message}
+                </Form.Control.Feedback>
+              )}
+              {errors?.password?.type === 'minLength' && (
+                <Form.Control.Feedback type="invalid">
+                  Password should be 6 characters.
+                </Form.Control.Feedback>
+              )}
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
